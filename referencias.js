5c4224ec4fe28ec085c57a6a4d3215b21f4aa968
Vitor Souza da Silva <39885926+Vitor545@users.noreply.github.com>
2022-05-20 11:51:08 -0300

    Create referencias.js

---
+++ referencias.js
@@ -0,0 +1,27 @@
+// SOURCE DE REFERENCIAS UTILIZADAS PARA O FRONT-END
+
+// https://contrastchecker.com/
+
+// https://tichung.com/blog/2021/20200323_flask/
+
+// https://www.youtube.com/watch?v=y8ckoDFXdVE
+
+// https://getbootstrap.com/docs/4.0/components/buttons/
+
+// https://www.zekagraphic.com/how-to-use-contrast-in-graphic-design/#:~:text=Color%20contrast%20in%20graphic%20design,a%20focal%20point%20adding%20the
+
+
+// https://pt-br.reactjs.org/docs/components-and-props.html
+
+// https://v5.reactrouter.com/web/api/Redirect
+
+
+// https://medium.com/@hardimaneric/full-crud-with-fetch-and-javascript-4e6c43080261
+
+// https://axios-http.com/docs/post_example
+
+
+// https://www.freecodecamp.org/portuguese/news/tutorial-de-react-router-como-usar-render-redirect-switch-link-e-mais-com-exemplos-de-codigo/
+
+
+// https://linguinecode.com/post/react-componentdidupdate-vs-useeffect
